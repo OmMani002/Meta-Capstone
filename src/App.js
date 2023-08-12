c5f27f480e3b9d392e81b484338fbda4a18207b8
Om Mani Tripathi <76935835+OmMani002@users.noreply.github.com>
2023-08-12 18:33:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Navbar from "./components/Navbar";
 import Routing from "./components/Routing";
 import Footer from "./components/Footer";
-//import {  Routes, Route } from "react-router-dom";
-//import Booking from './components/pages/Booking';
 import './App.css';
 
 function App() {
